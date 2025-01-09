3b24a997bbe2109937e84d832c4e6608c69c634d
ManojUchiha02 <manoj.boya@terralogic.com>
2025-01-09 17:31:56 +0530

    updated python list

--- .venv/python/test9.py
+++ .venv/python/test9.py
@@ -1,4 +1,14 @@
-l = [1,2,3]
+"""01) Given an array nums of distinct integers, return all the possible Permutations. You can return the answer in any order.
+Example 1:
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+Example 2:
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+Example 3:
+Input: nums = [1]
+Output: [[1]]"""
+l = [int(x) for x in (input("enter list:")).split()]
 l1 = []
 l2 = []
 l3 = []
